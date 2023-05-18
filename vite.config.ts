adc6ea25c6d080dcc13800b2b99f1a9ae9afc1e7
Jomel Bautista <jomelgbautista@gmail.com>
2023-05-18 11:20:18 -0700

    type checker package installed and debugged build bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+    })
+  ],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src")}]
   },
